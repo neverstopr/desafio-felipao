7d786cafccb3496017b878e5248d44d6de064ac4
neverstopr <bboypaulinho_neverstop@outlook.com>
2024-08-19 20:26:34 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,33 @@
+let nomeDoHeroi = "leviatã"
+let nivelDoHeroi = 8400
+
+if (nivelDoHeroi <= 1000){
+    console.log(nomeDoHeroi + " está no nível Prata")
+}else if (nivelDoHeroi <= 2000){
+    console.log(nomeDoHeroi + " está no nível Bronze")
+}else if (nivelDoHeroi <= 5000){
+    console.log (nomeDoHeroi + " está no nível Prata")
+}else if (nivelDoHeroi <= 7000){
+    console.log(nomeDoHeroi + " está no nível Ouro")
+}else if (nivelDoHeroi <= 8000){
+    console.log (nomeDoHeroi + " está no nível Platina")
+}else if (nivelDoHeroi <= 9000){
+    console.log(nomeDoHeroi + " está no nível Ascendente")
+}else if (nivelDoHeroi <= 10000){
+    console.log(nomeDoHeroi + " está no nível de Imortal")
+}else if (nivelDoHeroi >= 10001){
+    console.log (nomeDoHeroi + " está no nível Radiante")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
